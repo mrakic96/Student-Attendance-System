2693b8c65fa1de4af484534edbe98d848efbb53d
anton156 <aviskic@gmail.com>
2020-08-28 12:12:58 +0200

    logout fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -136,6 +136,9 @@
                 {{ __('Logout') }}
               </p>
             </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+             @csrf
+            </form>
           </li>
             </ul>     
       </nav>
