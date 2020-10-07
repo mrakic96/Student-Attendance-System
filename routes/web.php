03cda56eb757693306f5c0023c031880d168e5bf
mrakic96 <matej.rakic96@gmail.com>
2020-10-07 19:45:47 +0200

    Deploy settings

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+URL::forceRootUrl('https://studenti.sum.ba/projekti/fsre/2020/g2');
+
 //Pocetni view - index page
 Route::get('/', function () {
     if(Auth::guest()) {
